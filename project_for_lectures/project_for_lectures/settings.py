88eace0568cc1689f12e6be0920f744679b9a0ef
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2024-11-22 22:41:47 +0300

    Start uploading to server.

--- project_for_lectures/project_for_lectures/settings.py
+++ project_for_lectures/project_for_lectures/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,14 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rnl_a$5)1!)nqlc!9942$k=+ij)(ha!px3^8&12!j^#*$+lcg@'
+SECRET_KEY = os.getenv('SECRET_KEY')
+# 'django-insecure-rnl_a$5)1!)nqlc!9942$k=+ij)(ha!px3^8&12!j^#*$+lcg@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    '192.168.31.214',
+    # '127.0.0.1',
+    # '192.168.31.214',
+    'daniilKoblov.pythonanywhere.com',
 ]
 
 INTERNAL_IPS = [
@@ -90,8 +94,15 @@ WSGI_APPLICATION = 'project_for_lectures.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'daniilKoblov$default',
+        'USER': 'daniilKoblov',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'daniilKoblov.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
@@ -128,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
